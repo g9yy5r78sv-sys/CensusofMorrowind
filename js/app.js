e3933f3ce07fbed0a9071db1ad6bef48d640f0e9
g9yy5r78sv-sys <g9yy5r78sv@privaterelay.appleid.com>
2026-09-22 22:32:40 -0500

    Add Faction Matches section to character display

--- js/app.js
+++ js/app.js
@@ -268,6 +268,8 @@ function buildCard(b,title="Census Record"){
   <div>${esc(b.cls)}</div>
 </div>
 
+ <div class="row"><b>Faction Matches</b><span>${b.factions.map(esc).join(", ")}</span></div>
+
  <div class="characterStatsGrid">
 
   <div class="startingAttributes">
@@ -353,8 +355,6 @@ function buildCard(b,title="Census Record"){
 </div>
 
  <h3>Starting Spells</h3>${b.spells.length?b.spells.map(sp=>`<div class="row"><b>${sp.name}</b><span>${sp.desc} • ${sp.cost} magicka</span></div>`).join(""):"<p class=\"muted\">None granted</p>"}
-
- <div class="row"><h3>Faction Matches</h3><span>${b.factions.map(esc).join(", ")}</span></div>
 
  <p><button type="button" data-action="copy" data-copy="${esc(formatBuild(b))}">Copy Build</button></p></div>`;
 }
